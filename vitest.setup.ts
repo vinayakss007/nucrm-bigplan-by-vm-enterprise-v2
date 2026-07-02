bb898cfa649d61d4cbe917294db74004118ed57e
vinayakss007 <vinayakss007@users.noreply.github.com>
2026-07-02 04:22:07 +0000

    fix: add ENCRYPTION_KEY to test defaults for env validation tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -29,3 +29,6 @@ if (!process.env['SESSION_SECRET']) {
 if (!process.env['NEXT_PUBLIC_APP_URL']) {
   process.env['NEXT_PUBLIC_APP_URL'] = 'http://localhost:3000';
 }
+if (!process.env['ENCRYPTION_KEY']) {
+  process.env['ENCRYPTION_KEY'] = '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef';
+}
